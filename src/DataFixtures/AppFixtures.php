75f4fc28d0cd1728077e5a539240268152fbc0f0
Axel <axel.blanchardon@gmail.com>
2020-10-03 02:21:10 +0200

    fix error fixture load

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,7 +10,7 @@ use App\Entity\Categorie;
 use App\Entity\Utilisateur;
 use App\Entity\QuestionLike;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
